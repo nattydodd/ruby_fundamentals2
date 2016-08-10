b7fe48d1dc7d4316812b70a586b3b8a60b04da36
Natalie Dodd <nataliedodd89@gmail.com>
2016-08-10 15:38:11 -0400

    Creating a method for list printing

--- exercise6.rb
+++ exercise6.rb
@@ -3,3 +3,15 @@ grocery_list = ["bananas", "chocolate", "raspberries", "oatmeal"]
 grocery_list.each do |item|
   puts "* #{item}"
 end
+
+#Forgot an item, need to add to list
+grocery_list << "rice"
+
+#Creating a method for list printing
+def list_print(x)
+    x.each do |item|
+    puts "* #{item}"
+  end
+end
+
+new_list = list_print(grocery_list)
