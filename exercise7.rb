f524ad1c4e7fd1f40a7078d87e44dbca10549307
Natalie Dodd <nataliedodd89@gmail.com>
2016-08-10 16:32:03 -0400

    adding cohort4

--- exercise7.rb
+++ exercise7.rb
@@ -4,11 +4,15 @@ students = {
   :cohort2 => 42,
   :cohort3 => 22,
 }
-
+#method for desplaying hash
 def student_count(hash)
     hash.each do |x, y|
       puts "#{x}: #{y} students"
     end
 end
+#display student count
+student_count(students)
 
+#Adding cohort 4
+students[:cohort4] = "43"
 student_count(students)
